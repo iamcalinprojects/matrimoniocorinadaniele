ed48911838d195d8d2079597dffbec69c5e87585
c.anghelus <c.anghelus@easy-net.it>
2026-06-30 15:23:31 +0200

    aggiornata immagine la nostra giornata

--- src/Stich.jsx
+++ src/Stich.jsx
@@ -3,7 +3,7 @@ import heroImage from "./assets/IMG_3658.jpg";
 import backgroundImg from "./assets/Backgroundmatrimonio.png";
 import bgMatrimonio from "./assets/Backgroundmatrimonio.png";
 import bgilnostro from "./assets/bgIlnostro.png";
-import laNostraGiornata from "./assets/bgImgLaNostraGiornata.png";
+import laNostraGiornata from "./assets/BgLaNostraGiornatabig.png";
 import chiesaimg from "./assets/chiesaimg.png";
 import ricevimentoimg from "./assets/ricevimentoimg.png";
 import bgUpNostraGiornata from "./assets/bgUpNostraGiornata.png";
